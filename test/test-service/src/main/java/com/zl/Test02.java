b256ec7fa9a1120983b673e672ddfd21a0a2e1de
zl <13641296025@163.com>
2019-03-31 02:29:15 +0800

    commit

---
+++ test/test-service/src/main/java/com/zl/Test02.java
@@ -0,0 +1,8 @@
+package com.zl;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		System.out.println("test02");
+	}
+}
---
+++ test/test-service/src/main/java/com/zl/Test03.java
@@ -0,0 +1,8 @@
+package com.zl;
+
+public class Test03 {
+
+	public static void main(String[] args) {
+		System.out.println("test03");
+	}
+}
