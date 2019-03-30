7627a8f61e9de868301be3e6915f2ed1f3a364f0
zl <13641296025@163.com>
2019-03-31 00:08:32 +0800

    commit

---
+++ test-git/src/main/java/com/zl/service/Test.java
@@ -0,0 +1,11 @@
+package com.zl.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+	public static void main(String[] args) {
+		List<Object> list = new ArrayList<>();
+	}
+}
